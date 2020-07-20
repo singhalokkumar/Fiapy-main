e3e7f84ffb18e7e400b7b994e396e4c1c6fbfab3
Aditya Raj <adityaraj61522@gmail.com>
2020-07-20 21:48:40 +0530

    ad

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $db_name = "fiapy";gvchg
 
 $db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8',$db_user,$db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
+?>ghdctyfg
 
 
 <!-- INSERT INTO `service` (`id`, `title`, `description`) VALUES ('1', 'Electrician', 'Install, maintain, and repair electrical wiring, equipment, and fixtures.'); -->
