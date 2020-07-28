9e3ce858f7c46fcc5797a0d5fa56b4bc0e0b1026
Viper <iitjeeabhishek@gmail.com>
2020-07-28 16:47:29 +0530

    Added

---
+++ verifyOtp.php
@@ -0,0 +1,35 @@
+<!-- API Call For Verification -->
+<?php
+			$otp1 = false;
+			if(isset($_POST['Login'])) {
+				require('textlocal.class.php');
+				
+
+				$textlocal = new Textlocal(false, false, '/Qt6x9ynjgk-P1o43ptd51IgO4YjyvyyVvzWC62YSj');
+
+                $numbers = array($_POST['mobile']);
+
+				$sender = 'TXTLCL';
+				$otp = mt_rand(10000, 99999);
+				$message = "Your One Time Password(OTP) To Reset Your Password Is: "  . $otp;
+
+				try {
+				    $result = $textlocal->sendSms($numbers, $message, $sender);
+				    setcookie('otp', $otp);
+				    // echo "OTP Successfully Send..";
+				} catch (Exception $e) {
+				    die('Error: ' . $e->getMessage());
+				}
+			}
+
+			if(isset($_POST['otpsubmit'])) { 
+				$otp = $_POST['OTP'];
+				if($_COOKIE['otp'] == $otp) {
+					// echo "Congratulation, Your mobile is verified.";
+					header('location: login1.php');
+				} else {
+					// echo "Please Enter Correct OTP.";
+					$otp1 = true;
+				}
+			}
+		?>
